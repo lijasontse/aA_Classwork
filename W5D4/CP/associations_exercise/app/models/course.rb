ca75ee65ad25436e60f5c1f026209217363ddbe0
Jason Li <li.jasontse@gmail.com>
2021-02-22 19:14:31 -0800

    add W5D4 project

--- W5D4/CP/associations_exercise/app/models/course.rb
+++ W5D4/CP/associations_exercise/app/models/course.rb
@@ -10,20 +10,35 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
-  has_many :enrollments,
-    primary_key: :id,
-    foreign_key: :course_id,
-    class_name: :Enrollment
-
-  has_many :enrolled_students, through: :enrollments, source: :user
 
   belongs_to :prerequisite,
-    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+  
+  has_many :prerequisites,
     foreign_key: :prereq_id,
     class_name: :Course
 
   belongs_to :instructor,
-    primary_key: :id,
     foreign_key: :instructor_id,
     class_name: :User
+
 end
+
+
+# has_many :enrollments,
+#   primary_key: :id,
+#   foreign_key: :course_id,
+#   class_name: :Enrollment
+
+# has_many :enrolled_students, through: :enrollments, source: :user
+
+# belongs_to :prerequisite,
+#   primary_key: :id,
+#   foreign_key: :prereq_id,
+#   class_name: :Course
+
+# belongs_to :instructor,
+#   primary_key: :id,
+#   foreign_key: :instructor_id,
+#   class_name: :User
--- W5D4/CP/associations_exercise/app/models/user.rb
+++ W5D4/CP/associations_exercise/app/models/user.rb
@@ -9,15 +9,20 @@
 #
 class User < ApplicationRecord
 
-  has_many :enrollments,
-    primary_key: :id,
-    foreign_key: :student_id,
-    class_name: :Enrollment
-
-  has_many :enrolled_courses, through: :enrollments, source: :course
-
   has_many :instructor_courses,
-    primary_key: :id,
     foreign_key: :instructor_id,
     class_name: :Course
 end
+
+
+  # has_many :enrollments,
+  #   primary_key: :id,
+  #   foreign_key: :student_id,
+  #   class_name: :Enrollment
+
+  # has_many :enrolled_courses, through: :enrollments, source: :course
+
+  # has_many :instructor_courses,
+  #   primary_key: :id,
+  #   foreign_key: :instructor_id,
+  #   class_name: :Course
