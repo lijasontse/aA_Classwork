7546c0189cfe806fd82480c4fd6ff281276da310
Jason Li <li.jasontse@gmail.com>
2021-02-23 17:32:20 -0800

    add build your own active record project

---
+++ W6D2/ActiveRecordP1/lib/00_attr_accessor_object.rb
@@ -0,0 +1,14 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |method_name|
+      define_method(method_name) do 
+        instance_variable_get("@#{method_name}")
+      end
+
+      define_method("#{method_name}=") do |value|
+        instance_variable_set("@#{method_name}", value)
+      end
+  
+    end
+  end
+end
